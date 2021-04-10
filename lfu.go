8ae78aef7762cc4952da5529845113185231f4f1
Mohamed Shapan <m7shapan@gmail.com>
2021-04-10 18:36:13 +0200

    create Put to save to redis and Get to return it from redis and ZIncr by 1

--- lfu.go
+++ lfu.go
@@ -1,6 +1,13 @@
 package lfu
 
-import "github.com/go-redis/redis/v8"
+import (
+	"context"
+
+	"github.com/go-redis/redis/v8"
+)
+
+const LFUHashName = "LFUCacheHash"
+const LFUSortedSetName = "LFUCacheSortedSet"
 
 type LFU struct {
 	redisClient *redis.Client
@@ -10,4 +17,29 @@ func New(redisClient *redis.Client) *LFU {
 	return &LFU{
 		redisClient: redisClient,
 	}
+}
+
+func (c *LFU) Put(key string, value interface{}) (err error) {
+	err = c.redisClient.HSet(context.Background(), LFUHashName, key, value).Err()
+
+	c.redisClient.ZAdd(context.Background(), LFUSortedSetName, &redis.Z{
+		Member: key,
+		Score:  1,
+	})
+
+	return
+}
+
+func (c *LFU) Get(key string) (value string, err error) {
+	value, err = c.redisClient.HGet(context.Background(), LFUHashName, key).Result()
+
+	if err != nil {
+		return
+	}
+
+	c.redisClient.ZIncr(context.Background(), LFUSortedSetName, &redis.Z{
+		Member: key,
+		Score:  1,
+	})
+	return
 }
